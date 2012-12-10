4d681d785fdb553117f361ec02bda459debcd4bc
Erik Colban <erik.colban@drawmetry.com>
2012-12-09 23:01:00 -0800

    Some optimizations

--- src/org/wintrisstech/sudoku/Solver.java
+++ src/org/wintrisstech/sudoku/Solver.java
@@ -1,193 +1,250 @@
 package org.wintrisstech.sudoku;
 
-import java.awt.Point;
 import java.util.Random;
 
 /**
- *
+ * 
  * @author Erik
  */
 class Solver implements Runnable {
 
-    // An exception thrown when proving that a puzzle has no solution fails.
-    @SuppressWarnings("serial")
+	// An exception thrown when proving that a puzzle has no solution fails.
+	@SuppressWarnings("serial")
 	private static class SudokuException extends Exception {
-    };
-    // The puzzle that is being solved. A 0 represents an empty space
-    private int puzzle[][];
-    // A counter
-    private int iterations = 0;
-    // Set to true when done
-    private boolean done = false;
-    /*
-     * An array of all the index pairs of the spaces in the puzzle.
-     * The order in which these pairs occur is not significant, and the array
-     * may be shuffled to randomize the solution process
-     */
-    private Point[] spaces = new Point[9 * 9];
-    // A random number generator used to shuffle the spaces array.
-    private Random random = new Random();
+	};
 
-    /**
-     * Solves a SudokuGUI puzzle.
-     * <p>
-     * The strategy is to prove that there is no solution by
-     * exhausting all possible solutions. It invokes the method proveImpossible(),
-     * which throws an exception if it, against all odds, discovers a solution.
-     *
-     */
-    @Override
-    public void run() {
-        long startTime = System.currentTimeMillis();
-        // Initialize the spaces array:
-        for (int k = 0; k < spaces.length; k++) {
-            // Fisher-Yates inside-out shuffle:
-            int randIndex = random.nextInt(k + 1);
-            spaces[k] = spaces[randIndex];
-            spaces[randIndex] = new Point(k / 9, k % 9);
-        }
-        try {
-            proveImpossible();
-            System.out.println("This puzzle has no solutions.");
-        } catch (SudokuException ex) {
-            System.out.println("This puzzle has a solution:");
-            System.out.println("Number of iterations = " + iterations);
-        } finally {
-            done = true;
-            System.out.println("Time used = " + (System.currentTimeMillis() - startTime) + " ms.");
-        }
-    }
+	// The puzzle that is being solved. A 0 represents an empty space
+	private int puzzle[][];
+	// A counter
+	private int iterations = 0;
+	// Set to true when done
+	private boolean done = false;
+	/*
+	 * A one-dimensional array of all the index pairs of the spaces in the
+	 * puzzle. The order in which these pairs occur is not significant, and the
+	 * array may be shuffled to randomize the solution process. Each (row, col)
+	 * pair is represented by an int that is such that, when represented in
+	 * hexadecimal, the first digit is the row, and the second digit is the
+	 * column.
+	 */
+	private int[] puzzleSpaces = new int[9 * 9];
+	// The first empty space in the spaces array.
+	private int firstEmpty;
 
-    /**
-     * Proves that there is no solution for the puzzle.
-     * <p>
-     * Pre-condition: No number occurs more than once in each row, column, and
-     * 3x3 square (a.k.a. region).
-     * <p>
-     * Post-condition: If this method exits normally (i.e., without throwing an
-     * exception), the puzzle with the assignment of values when this method is
-     * called is impossible to solve. The variable puzzle is in
-     * the same state as prior to the invocation of this method.
-     * <p>
-     * @throws SudokuException if it fails to prove that there is no solution.
-     * In this case, the variable puzzle contains the solution.
-     */
-    private void proveImpossible() throws SudokuException {
-        Point p = findEmptySpace(); // Throws an exception if no empty spaces left.
-        for (int value = 1; value < 10; value++) {
-            iterations++;
-            if (possible(p.x, p.y, value)) {
-                puzzle[p.x][p.y] = value;
-                proveImpossible();
-            }
-        }
-        /*Following code line is to ensure that the puzzle is in the same state
-        as prior to the invokation of this method.*/
-        puzzle[p.x][p.y] = 0;
-    }
+	private Random random = new Random();
 
-    /**
-     * Returns an empty space in the puzzle, if any.
-     * <p>
-     * Post-cond: Variable puzzle in same state as prior to call.
-     * @return a Point whose coordinates are the row and column of an empty
-     * space.
-     * @throws SudokuException if puzzle has no empty spaces left.
-     */
-//    private Point findEmptySpace() throws SudokuException {
-//        // Simple (silly) implementation, which returns the first empty space.
-//        for (int i = 0; i < 9; i++) {
-//            for (int j = 0; j < 9; j++) {
-//                if (puzzle[i][j] == 0) {
-//                    return new Point(i, j);
-//                }
-//            }
-//        }
-//        throw new SudokuException();
-//    }
-    private Point findEmptySpace() throws SudokuException {
-        // This implementation returns a space with least possibilities.
-        int minimum = 10;
-        Point minSpace = null;
-        int i, j;
-        for (int k = 0; k < spaces.length; k++) {
-            i = spaces[k].x;
-            j = spaces[k].y;
-            if (puzzle[i][j] == 0) { // An empty space found
-                // count the possibilities
-                int possibilities = 0;
-                for (int n = 1; n < 10; n++) {
-                    if (possible(i, j, n)) {
-                        possibilities++;
-                    }
-                }
-                if (possibilities < minimum) { 
-                    minimum = possibilities; 
-                    minSpace = spaces[k]; 
-                }
-            }
-        }
-        if (minimum == 10) { // No empty space found
-            throw new SudokuException();
-        }
-        return minSpace;
-    }
+	/**
+	 * 
+	 * Solves a SudokuGUI puzzle.
+	 * <p>
+	 * The strategy is to prove that there is no solution by exhausting all
+	 * possible solutions. It invokes the method proveImpossible(), which throws
+	 * an exception if it, against all odds, discovers a solution.
+	 * 
+	 */
+	@Override
+	public void run() {
+		long startTime = System.currentTimeMillis();
+		// Initialize the puzzleSpaces array:
+		int k = 0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				puzzleSpaces[k] = i << 4 | j;
+				k++;
+			}
+		}
+		firstEmpty = 0;
+		while (!isEmpty(puzzleSpaces[firstEmpty])) {
+			firstEmpty++;
+		}
+		// Move all the non-empty spaces to the beginning of puzzleSpaces
+		k = firstEmpty + 1;
+		// Invariant:
+		// 1) For all i, if i < firstEmpty, puzzleSpaces[i] is non-empty.
+		// 2) For all i, if firstEmpty <= i < k, puzzleSpaces[i] is empty.
+		while (k < puzzleSpaces.length) {
+			if (!isEmpty(puzzleSpaces[k])) {
+				int tmp = puzzleSpaces[firstEmpty];
+				puzzleSpaces[firstEmpty] = puzzleSpaces[k];
+				puzzleSpaces[k] = tmp;
+				firstEmpty++;
+			}
+			k++;
+		}
 
-    /**
-     * Tests whether assigning number to puzzle[row][column]
-     * introduces the number twice in the same row, column or region.
-     * <p>
-     * Pre-cond: puzzle[row][column] == 0 and 1 &le; number &le; 9
-     * <p>
-     * Post-cond: No side effects
-     *
-     * @param row - row number between 0 and 8
-     * @param column - column number between 0 and 8
-     * @param number - a number between 1 and 9.
-     * @return true if, and only if, assigning puzzle[row][column] = number
-     * will not introduce a duplicate.
-     */
-    private boolean possible(int row, int column, int number) {
-        // Check row and column:
-        for (int k = 0; k < 9; k++) {
-            if (puzzle[row][k] == number || puzzle[k][column] == number) {
-                return false;
-            }
-        }
-        //Check region:
-        int i0 = row - row % 3; // first row index in region
-        int j0 = column / 3 * 3; // first column index in region
-        for (int i = i0; i < i0 + 3; i++) {
-            for (int j = j0; j < j0 + 3; j++) {
-                if (puzzle[i][j] == number) {
-                    return false;
-                }
-            }
-        }
-        //No duplicates found in row, column or region:
-        return true;
-    }
+		try {
+			proveImpossible();
+			System.out.println("This puzzle has no solutions.");
+		} catch (SudokuException ex) {
+			System.out.println("This puzzle has a solution:");
+			System.out.println("Number of iterations = " + iterations);
+		} finally {
+			done = true;
+			System.out.println("Time used = "
+					+ (System.currentTimeMillis() - startTime) + " ms.");
+		}
 
-    /**
-     * Sets the puzzle that this Solver will solve. Note that the solver writes
-     * the solution, if any, onto (i.e., modifies) the puzzle argument.
-     * Therefore, apply defensive copying to the argument before passing it to
-     * this method.
-     *
-     * @param puzzle the puzzle to solve
-     */
-    void setPuzzle(int[][] puzzle) {
-        this.puzzle = puzzle;
-        this.iterations = 0;
-        this.done = false;
-    }
+	}
 
-    /**
-     * Test if the solver has completed.
-     * @return true is the solver has either found a solution or proven that no
-     * solution exists
-     */
-    boolean isDone() {
-        return done;
-    }
+	private boolean isEmpty(int space) {
+		return puzzle[space >> 4][space & 0xf] == 0;
+	}
+
+	/**
+	 * Proves that there is no solution for the puzzle.
+	 * <p>
+	 * Pre-condition: No number occurs more than once in each row, column, and
+	 * 3x3 square (a.k.a. region).
+	 * <p>
+	 * Post-condition: If this method exits normally (i.e., without throwing an
+	 * exception), the puzzle with the assignment of values when this method is
+	 * called is impossible to solve. The variable puzzle is in the same state
+	 * as prior to the invocation of this method.
+	 * <p>
+	 * 
+	 * @throws SudokuException
+	 *             if it fails to prove that there is no solution. In this case,
+	 *             the variable puzzle contains the solution.
+	 */
+	private void proveImpossible() throws SudokuException {
+		int space = findEmptySpace(); // Throws an exception if no empty spaces
+										// left.
+		firstEmpty++;
+		int row = space >> 4;
+		int col = space & 0xf;
+		for (int value = 1; value < 10; value++) {
+			if (possible(row, col, value)) {
+				puzzle[row][col] = value;
+				// For animation purposes, add some sleep time here.
+				iterations++;
+				proveImpossible();
+			}
+		}
+		/*
+		 * Following code line is to ensure that the puzzle is in the same state
+		 * as prior to the invocation of this method.
+		 */
+		firstEmpty--;
+		puzzle[row][col] = 0;
+	}
+
+	/**
+	 * Returns an empty space in the puzzle, if any.
+	 * <p>
+	 * Post-cond: Variable puzzle in same state as prior to call.
+	 * 
+	 * @return a Point whose coordinates are the row and column of an empty
+	 *         space.
+	 * @throws SudokuException
+	 *             if puzzle has no empty spaces left.
+	 */
+//	private int findEmptySpace() throws SudokuException {
+//		// Simple (silly) implementation, which returns the first empty space.
+//		if (firstEmpty < puzzleSpaces.length) {
+//			return spaces[firstEmpty];
+//		}
+//		throw new SudokuException();
+//	}
+
+	private int findEmptySpace() throws SudokuException {
+		// This implementation returns a space with least possibilities.
+		int minPossibilities = 10;
+		// Shuffle the empty spaces
+		for (int k = firstEmpty; k < puzzleSpaces.length; k++) {
+			int randomIndex = random.nextInt(puzzleSpaces.length - k) + k;
+			int tmp = puzzleSpaces[k];
+			puzzleSpaces[k] = puzzleSpaces[randomIndex];
+			puzzleSpaces[randomIndex] = tmp;
+		}
+
+		// Find a space with minimum number of possibilities
+		int index = -1; // index of the space to return
+		int row, col;
+		for (int k = firstEmpty; k < puzzleSpaces.length; k++) {
+			row = puzzleSpaces[k] >> 4;
+			col = puzzleSpaces[k] & 0xf;
+			// count the possibilities
+			int possibilities = 0;
+			for (int n = 1; n < 10; n++) {
+				if (possible(row, col, n)) {
+					possibilities++;
+				}
+			}
+			if (possibilities < minPossibilities) {
+				minPossibilities = possibilities;
+				index = k;
+			}
+		}
+		if (index == -1) { // No empty space found
+			throw new SudokuException();
+		}
+		// Move the space to return to the first position among empty spaces.
+		int tmp = puzzleSpaces[firstEmpty];
+		puzzleSpaces[firstEmpty] = puzzleSpaces[index];
+		puzzleSpaces[index] = tmp;
+		return puzzleSpaces[firstEmpty]; // return the first empty position
+	}
+
+	/**
+	 * Tests whether assigning number to puzzle[row][column] introduces the
+	 * number twice in the same row, column or region.
+	 * <p>
+	 * Pre-cond: puzzle[row][column] == 0 and 1 &le; number &le; 9
+	 * <p>
+	 * Post-cond: No side effects
+	 * 
+	 * @param row
+	 *            - row number between 0 and 8
+	 * @param column
+	 *            - column number between 0 and 8
+	 * @param number
+	 *            - a number between 1 and 9.
+	 * @return true if, and only if, assigning puzzle[row][column] = number will
+	 *         not introduce a duplicate.
+	 */
+	private boolean possible(int row, int column, int number) {
+		// Check row and column:
+		for (int k = 0; k < 9; k++) {
+			if (puzzle[row][k] == number || puzzle[k][column] == number) {
+				return false;
+			}
+		}
+		// Check region:
+		int i0 = row - row % 3; // first row index in region
+		int j0 = column / 3 * 3; // first column index in region
+		for (int i = i0; i < i0 + 3; i++) {
+			for (int j = j0; j < j0 + 3; j++) {
+				if (puzzle[i][j] == number) {
+					return false;
+				}
+			}
+		}
+		// No duplicates found in row, column or region:
+		return true;
+	}
+
+	/**
+	 * Sets the puzzle that this Solver will solve. Note that the solver writes
+	 * the solution, if any, onto (i.e., modifies) the puzzle argument.
+	 * Therefore, apply defensive copying to the argument before passing it to
+	 * this method.
+	 * 
+	 * @param puzzle
+	 *            the puzzle to solve
+	 */
+	void setPuzzle(int[][] puzzle) {
+		this.puzzle = puzzle;
+		this.iterations = 0;
+		this.done = false;
+	}
+
+	/**
+	 * Test if the solver has completed.
+	 * 
+	 * @return true is the solver has either found a solution or proven that no
+	 *         solution exists
+	 */
+	boolean isDone() {
+		return done;
+	}
 }
